f2243a70dfd4598a1806606e1df083fb977f7c5b
Brad <brthiess@ualberta.ca>
2015-03-13 23:14:02 -0600

    Changes to Skills section
    Signed-off-by: Brad <brthiess@ualberta.ca>

--- skills.php
+++ skills.php
@@ -5,106 +5,49 @@
 	<div class=" col-sm-8 col-sm-offset-2">
 		<img class="center-block img-responsive" src="images/gray-line.png">	<br><br>
 	</div>
-
-	<div class="col-sm-12">
-		<div class="col-lg-3 col-md-4 col-sm-6">
-			<div class="chart-holder center-block">
-				<canvas id="javascript-chart" width="150" height="150"></canvas>
-				<div class="percentage">
-					75%
-				</div>
-			</div>
-			<div class="skill text-center">
-			Javascript
-			</div>
-		</div>
-		<div class="col-lg-3 col-md-4 col-sm-6">
-			<div class="chart-holder center-block">
-				<canvas id="bootstrap-chart" width="150" height="150"></canvas>
-				<div class="percentage">
-					90%
-				</div>
-			</div>
-			<div class="skill text-center">
-			Bootstrap
-			</div>
-		</div>
-		<div class="col-lg-3 col-md-4 col-sm-6">
-			<div class="chart-holder center-block">
-				<canvas id="jquery-chart" width="150" height="150"></canvas>
-				<div class="percentage">
-					70%
-				</div>
-			</div>
-			<div class="skill text-center">
-			jQuery
-			</div>
-		</div>
-		<div class="col-lg-3 col-md-4 col-sm-6">
-			<div class="chart-holder center-block">
-				<canvas id="html-css-chart" width="150" height="150"></canvas>
-				<div class="percentage">
-					80%
-				</div>
-			</div>
-			<div class="skill text-center">
-			HTML/CSS
-			</div>
-		</div>
-		<div class="col-lg-3 col-md-4 col-sm-6">
-			<div class="chart-holder center-block">
-				<canvas id="php-chart" width="150" height="150"></canvas>
-				<div class="percentage">
-					70%
-				</div>
-			</div>
-			<div class="skill text-center">
-			PHP
-			</div>
-		</div>
-		<div class="col-lg-3 col-md-4 col-sm-6">
-			<div class="chart-holder center-block">
-				<canvas id="ajax-chart" width="150" height="150"></canvas>
-				<div class="percentage">
-					60%
-				</div>
-			</div>
-			<div class="skill text-center">
-			AJAX
-			</div>
-		</div>
-		<div class="col-lg-3 col-md-4 col-sm-6">
-			<div class="chart-holder center-block">
-				<canvas id="mysql-chart" width="150" height="150"></canvas>
-				<div class="percentage">
-					75%
-				</div>
-			</div>
-			<div class="skill text-center">
-			MySQL
-			</div>
-		</div>
-		<div class="col-lg-3 col-md-4 col-sm-6">
-			<div class="chart-holder center-block">
-				<canvas id="python-chart" width="150" height="150"></canvas>
-				<div class="percentage">
-					85%
-				</div>
-			</div>
-			<div class="skill text-center">
-			Python
-			</div>
+	
+	<div class="col-sm-4 skill-type">
+		<div class="skill-card col-sm-12 center-block">
+		  <div class="front face square">
+			<p class="left"><</p>
+			<img src="images/html5.png"><p class="right">></p>
+		  </div>
+		  <div class="back face square">
+			<div class="mini-skill one left-skill"><p><img src="images/html.png">&nbsp&nbspHTML</p></div><div class="mini-skill five right-skill"><p><img src="images/css.png">CSS</p></div>
+			<div class="mini-skill two left-skill"><p><img src="images/jquery.png">&nbsp jQuery</p></div><div class="mini-skill six right-skill"><p><img src="images/mysql.png">&nbsp AJAX</p></div>
+			<div class="mini-skill three"><p><img src="images/javascript.png"> &nbsp&nbspJavascript</p></div>
+			<div class="mini-skill four left-skill"><p>&nbsp Bootstrap</p></div><div class="mini-skill seven right-skill"><p><img src="images/php.png"> PHP</p></div>
+		  </div>
 		</div>
 	</div>
-	
-	<script> 
-	$( document ).ready(function() { insertChart("javascript-chart", 75);});
-	$( document ).ready(function() { insertChart("jquery-chart", 70);});
-	$( document ).ready(function() { insertChart("html-css-chart", 80);});
-	$( document ).ready(function() { insertChart("python-chart", 85);});
-	$( document ).ready(function() { insertChart("mysql-chart", 75);});
-	$( document ).ready(function() { insertChart("bootstrap-chart", 90);});
-	$( document ).ready(function() { insertChart("php-chart", 70);});
-	$( document ).ready(function() { insertChart("ajax-chart", 60);});
-	</script>
+	<div class="col-sm-4 skill-type">
+		<div class="skill-card col-sm-12 center-block">
+		  <div class="front face square">
+			<p class="left"><</p>
+			<img src="images/software.png"><p class="right">></p>
+		  </div>
+		  <div class="back face square">
+			<div class="mini-skill one left-skill"><p><img src="images/html.png">&nbsp&nbsp C#</p></div><div class="mini-skill five right-skill"><p><img src="images/css.png">C/C++</p></div>
+			<div class="mini-skill two left-skill"><p><img src="images/jquery.png">&nbsp Java</p></div><div class="mini-skill six right-skill"><p><img src="images/mysql.png">&nbsp MySQL </p></div>
+			<div class="mini-skill three"><p><img src="images/javascript.png"> &nbsp&nbsp Oracle DB</p></div>
+			<div class="mini-skill four left-skill"><p>&nbsp Python</p></div><div class="mini-skill seven right-skill"><p><img src="images/php.png"> </p></div>
+		  </div>
+		</div>
+	</div>
+	<div class="col-sm-4 skill-type">
+		<div class="skill-card col-sm-12 center-block">
+		  <div class="front face square">
+			<p class="left"><</p>
+			<img src="images/personality.png"><p class="right">></p>
+		  </div>
+		  <div class="back face square">
+			<h3 class="mini-skill one"><img src="images/logical.png">Logical</h3>
+			<h3 class="mini-skill two"><img src="images/creative.png">Creative</h3>
+			<h3 class="mini-skill three"><img src="images/diligent.png">Diligent</h3>
+			<h3 class="mini-skill four"><img src="images/friendly.png">Friendly</h3>
+		  </div>
+		</div>
+	</div>
+
+
 </div>
