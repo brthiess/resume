7f4b6b17665b5accf121c91bcfc861ab59416ed8
Brad <brthiess@ualberta.ca>
2015-03-14 14:02:47 -0600

    Fixed UI for Skills

--- skills.php
+++ skills.php
@@ -6,46 +6,56 @@
 		<img class="center-block img-responsive" src="images/gray-line.png">	<br><br>
 	</div>
 	
-	<div class="col-sm-4 skill-type">
-		<div class="skill-card col-sm-12 center-block">
-		  <div class="front face square">
-			<p class="left"><</p>
-			<img src="images/html5.png"><p class="right">></p>
-		  </div>
-		  <div class="back face square">
-			<div class="mini-skill one left-skill"><p><img src="images/html.png">&nbsp&nbspHTML</p></div><div class="mini-skill five right-skill"><p><img src="images/css.png">CSS</p></div>
-			<div class="mini-skill two left-skill"><p><img src="images/jquery.png">&nbsp jQuery</p></div><div class="mini-skill six right-skill"><p><img src="images/mysql.png">&nbsp AJAX</p></div>
-			<div class="mini-skill three"><p><img src="images/javascript.png"> &nbsp&nbspJavascript</p></div>
-			<div class="mini-skill four left-skill"><p>&nbsp Bootstrap</p></div><div class="mini-skill seven right-skill"><p><img src="images/php.png"> PHP</p></div>
-		  </div>
+	<div class="col-sm-4">
+		<div class="skill-background center-block">
+			<div class="garage">
+					<img class="up-arrow" src="images/up-arrow">
+				<div class="front door">
+					<img src="images/html5.png">
+				</div>
+				<div class="door">
+					<div class="mini-skill-container">
+						<div class="mini-skill one left-skill"><p>&nbsp<img src="images/html.png"> HTML</p></div><div class="mini-skill five right-skill"><p>&nbsp<img src="images/css.png"> CSS</p></div>
+						<div class="mini-skill two left-skill"><p>&nbsp<img src="images/jquery.png"> jQuery</p></div><div class="mini-skill six right-skill"><p>&nbsp<img src="images/ajax.png"> AJAX</p></div>
+						<div class="mini-skill three"><p>&nbsp<img src="images/javascript.png"> &nbsp&nbsp Javascript</p></div>
+						<div class="mini-skill four left-skill"><p>&nbsp Bootstrap</p></div><div class="mini-skill seven right-skill"><p>&nbsp<img src="images/php.png"> PHP</p></div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
-	<div class="col-sm-4 skill-type">
-		<div class="skill-card col-sm-12 center-block">
-		  <div class="front face square">
-			<p class="left"><</p>
-			<img src="images/software.png"><p class="right">></p>
-		  </div>
-		  <div class="back face square">
-			<div class="mini-skill one left-skill"><p><img src="images/html.png">&nbsp&nbsp C#</p></div><div class="mini-skill five right-skill"><p><img src="images/css.png">C/C++</p></div>
-			<div class="mini-skill two left-skill"><p><img src="images/jquery.png">&nbsp Java</p></div><div class="mini-skill six right-skill"><p><img src="images/mysql.png">&nbsp MySQL </p></div>
-			<div class="mini-skill three"><p><img src="images/javascript.png"> &nbsp&nbsp Oracle DB</p></div>
-			<div class="mini-skill four left-skill"><p>&nbsp Python</p></div><div class="mini-skill seven right-skill"><p><img src="images/php.png"> </p></div>
-		  </div>
+	<div class="col-sm-4">
+		<div class="skill-background center-block">
+			<div class="garage">
+				  <div class="front door">
+					<img src="images/software.png">
+				  </div>
+				<div class="door">
+					<div class="mini-skill-container">
+						<div class="mini-skill one left-skill"><p>&nbsp<img src="images/java.png">&nbsp&nbspJava</p></div><div class="mini-skill five right-skill"><p>&nbsp<img src="images/github.png"> GitHub</p></div>
+						<div class="mini-skill two left-skill"><p>&nbsp<img src="images/c-sharp.png">&nbspC#</p></div><div class="mini-skill six right-skill"><p>&nbsp<img src="images/mysql.png"> MySQL</p></div>
+						<div class="mini-skill three"><p>&nbsp<img src="images/oracledb.gif"> &nbsp&nbspOracle DB</p></div>
+						<div class="mini-skill four left-skill"><p>&nbsp<img src="images/python.png"> Python</p></div><div class="mini-skill seven right-skill"><p>&nbsp<img src="images/php.png"> C/C++</p></div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
-	<div class="col-sm-4 skill-type">
-		<div class="skill-card col-sm-12 center-block">
-		  <div class="front face square">
-			<p class="left"><</p>
-			<img src="images/personality.png"><p class="right">></p>
-		  </div>
-		  <div class="back face square">
-			<h3 class="mini-skill one"><img src="images/logical.png">Logical</h3>
-			<h3 class="mini-skill two"><img src="images/creative.png">Creative</h3>
-			<h3 class="mini-skill three"><img src="images/diligent.png">Diligent</h3>
-			<h3 class="mini-skill four"><img src="images/friendly.png">Friendly</h3>
-		  </div>
+	<div class="col-sm-4">
+		<div class="skill-background center-block">
+			<div class="garage">
+			  <div class="front door">
+				<img src="images/personality.png">
+			  </div>
+			  <div class="door">
+				<div class="mini-skill-container">
+						<div class="mini-skill one left-skill"><p><img src="images/logical.png">&nbsp&nbspLogical</p></div><div class="mini-skill five right-skill"><p>&nbsp<img src="images/creative.png"> Creative</p></div>
+						<div class="mini-skill two"><p>&nbsp<img src="images/passionate.png"> &nbsp&nbsp Passionate</p></div>
+						<div class="mini-skill three"><p>&nbsp<img src="images/knowledgeable.png"> &nbsp&nbsp Knowledgeable</p></div>
+						<div class="mini-skill four left-skill"><p>&nbsp<img src="images/diligent.png"> Diligent</p></div><div class="mini-skill seven right-skill"><p>&nbsp<img src="images/friendly.png"> Friendly</p></div>
+				</div>
+			  </div>
+			 </div>
 		</div>
 	</div>
 
